9b4a4ffbc7b85f32f690906ee76b4df31eb0eaaf
seckujdivad <juckes1david@gmail.com>
2019-10-14 09:57:00 +0100

    Wrote some utility methods for multiformviewer

--- tkforms.py
+++ tkforms.py
@@ -5,15 +5,19 @@ import abc
 class Form(tk.Frame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self.root = args[0]
     
     def __del__(self):
         pass
     
     def show(self):
-        pass
+        self._on_show()
+        self.pack(self.root)
 
     def hide(self):
-        pass
+        self._on_hide()
+        self.pack_forget()
 
     @abc.abstractclassmethod
     def _on_show(self):
@@ -29,9 +33,29 @@ class MultiFormViewer(Form):
         super().__init__(*args, **kwargs)
         
         self._forms = []
+        self._current_form = None
     
     def add_form(self, form):
-        self._forms.append(form)
+        if isinstance(form, Form):
+            self._forms.append(form)
+            return len(self._forms) - 1
+        
+        else:
+            raise TypeError('"form" must be of type Form, not {}'.format(type(form)))
     
     def remove_form(self, form):
-        self._forms.remove(form)
+        if type(form) == int:
+            self._forms.pop(form)
+
+        elif isinstance(form, Form):
+            self._forms.remove(form)
+        
+        else:
+            raise TypeError('"form" must be of type Form or int, not {}'.format(type(form)))
+    
+    def switch_to_form(self, index):
+        if self._current_form is not None:
+            self._forms[self._current_form].hide()
+
+        self._forms[index].show()
+        self._current_form = index
