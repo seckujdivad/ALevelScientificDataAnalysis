05cf9264f04b254fb51ea5b7f1f099c809cf7d69
seckujdivad <juckes1david@gmail.com>
2019-10-15 12:30:26 +0100

    Added outlines for future GUI components

--- Data Analyser.py
+++ Data Analyser.py
@@ -12,5 +12,27 @@ class MainForm(tkforms.Form):
         pass
 
 
+class GraphingForm(tkforms.Form):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+    def _on_show(self):
+        pass
+
+    def _on_hide(self):
+        pass
+
+
+class DataForm(tkforms.Form):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+    def _on_show(self):
+        pass
+
+    def _on_hide(self):
+        pass
+
+
 if __name__ == '__main__':
     app = tkforms.App(form = MainForm)
