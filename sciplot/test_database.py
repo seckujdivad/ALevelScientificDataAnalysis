e0b265ce473167d1afd7bd58187f61dddaa37859
seckujdivad <juckes1david@gmail.com>
2019-12-20 00:22:21 +0000

    Closed database in all database unit tests

--- sciplot/test_database.py
+++ sciplot/test_database.py
@@ -1,25 +1,31 @@
 import unittest
-import database
+
+import database #pylint: disable=import-error
+
 
 class TestDatabase(unittest.TestCase):
     def test_query(self):
         db = database.Database('../resources/test datasets/database.db')
         q = database.Query("SELECT * FROM TestTable", [], 0)
         db.query(q)
+        db.close()
     
     def test_insertion(self):
         db = database.Database('../resources/test datasets/database.db')
         q = database.Query("SELECT * FROM TestTable WHERE TestTableID = (?)", [1], 0)
         db.query(q)
+        db.close()
 
     def test_return_many(self):
         db = database.Database('../resources/test datasets/database.db')
         q = database.Query("SELECT * FROM TestTable WHERE Value = 1", [], 1)
         result = db.query(q)
         self.assertEqual(result, [[(1, 1, "Hello"), (2, 1, "World")]])
+        db.close()
     
     def test_return_one(self):
         db = database.Database('../resources/test datasets/database.db')
         q = database.Query("SELECT * FROM TestTable WHERE TestTableID = 1", [], 2)
         result = db.query(q)
         self.assertEqual(result, [(1, 1, "Hello")])
+        db.close()
