5f1f7d12c1892e9fcbbd3a3d183c1d8280537124
seckujdivad <juckes1david@gmail.com>
2019-12-27 22:57:54 +0000

    Wrote tests for uncertainty and units calculations

--- sciplot/test_functions.py
+++ sciplot/test_functions.py
@@ -75,6 +75,85 @@ class TestFunction(unittest.TestCase):
         for key in datatable:
             result[key] = functions.Value(datatable[key])
         return result
+    
+    def test_addition_uncertainty(self):
+        func = functions.Function('{g} + {g}')
+        result = func.evaluate(self.generic_datatable)
+        self.assertEqual(result.absolute_uncertainty, 0.02)
+    
+    def test_addition_units(self):
+        result = functions.Function('{g} + {g}').evaluate(self.generic_datatable).units
+        result.sort()
+        self.assertEqual(result, self.generic_datatable['g'].units)
+
+    def test_subtraction_uncertainty(self):
+        func = functions.Function('{g} - {mass}')
+        result = func.evaluate(self.generic_datatable)
+        self.assertEqual(result.absolute_uncertainty, 1.01)
+
+    def test_subtraction_units(self):
+        result = functions.Function('{g} - {g}').evaluate(self.generic_datatable).units
+        result.sort()
+        self.assertEqual(result, self.generic_datatable['g'].units)
+
+    def test_multiplication_uncertainty(self):
+        func = functions.Function('{g} * {mass}')
+        result = func.evaluate(self.generic_datatable)
+        self.assertEqual(result.percentage_uncertainty, self.generic_datatable['g'].percentage_uncertainty + self.generic_datatable['mass'].percentage_uncertainty)
+
+    def test_multiplication_units(self):
+        result = functions.Function('{g} * {g}').evaluate(self.generic_datatable).units
+        result.sort()
+        self.assertEqual(result, [(unit_id, power * 2) for unit_id, power in self.generic_datatable['g'].units])
+
+    def test_division_uncertainty(self):
+        func = functions.Function('{g} / {mass}')
+        result = func.evaluate(self.generic_datatable)
+        self.assertEqual(result.percentage_uncertainty, self.generic_datatable['g'].percentage_uncertainty + self.generic_datatable['mass'].percentage_uncertainty)
+
+    def test_division_units(self):
+        result = functions.Function('{g} / {g}').evaluate(self.generic_datatable).units
+        self.assertEqual(result, [])
+
+    def test_indices_uncertainty(self):
+        func = functions.Function('{g}^2')
+        result = func.evaluate(self.generic_datatable)
+        self.assertEqual(result.percentage_uncertainty, self.generic_datatable['g'].percentage_uncertainty * 2)
+
+    def test_indices_units(self):
+        result = functions.Function('{g}^2').evaluate(self.generic_datatable).units
+        result.sort()
+        self.assertEqual(result, [(unit_id, power * 2) for unit_id, power in self.generic_datatable['g'].units])
+
+    def test_misc_funcs_uncertainty(self):
+        self.assertEqual(functions.Function('sin{g}').evaluate(self.generic_datatable).percentage_uncertainty, self.generic_datatable['g'].percentage_uncertainty)
+        self.assertEqual(functions.Function('cos{g}').evaluate(self.generic_datatable).percentage_uncertainty, self.generic_datatable['g'].percentage_uncertainty)
+        self.assertEqual(functions.Function('tan{g}').evaluate(self.generic_datatable).percentage_uncertainty, self.generic_datatable['g'].percentage_uncertainty)
+        self.assertEqual(functions.Function('arcsin({g} / 10)').evaluate(self.generic_datatable).percentage_uncertainty, self.generic_datatable['g'].percentage_uncertainty)
+        self.assertEqual(functions.Function('arccos({g} / 10)').evaluate(self.generic_datatable).percentage_uncertainty, self.generic_datatable['g'].percentage_uncertainty)
+        self.assertEqual(functions.Function('arctan({g} / 10)').evaluate(self.generic_datatable).percentage_uncertainty, self.generic_datatable['g'].percentage_uncertainty)
+        self.assertEqual(functions.Function('rad{g}').evaluate(self.generic_datatable).percentage_uncertainty, self.generic_datatable['g'].percentage_uncertainty)
+        self.assertEqual(functions.Function('deg{g}').evaluate(self.generic_datatable).percentage_uncertainty, self.generic_datatable['g'].percentage_uncertainty)
+        self.assertEqual(functions.Function('abs{g}').evaluate(self.generic_datatable).percentage_uncertainty, self.generic_datatable['g'].percentage_uncertainty)
+        self.assertEqual(functions.Function('ln{g}').evaluate(self.generic_datatable).percentage_uncertainty, self.generic_datatable['g'].percentage_uncertainty)
+        self.assertEqual(functions.Function('log{g}').evaluate(self.generic_datatable).percentage_uncertainty, self.generic_datatable['g'].percentage_uncertainty)
+
+    def test_misc_funcs_units(self):
+        self.assertEqual(functions.Function('sin{g}').evaluate(self.generic_datatable).units, [])
+        self.assertEqual(functions.Function('cos{g}').evaluate(self.generic_datatable).units, [])
+        self.assertEqual(functions.Function('tan{g}').evaluate(self.generic_datatable).units, [])
+        self.assertEqual(functions.Function('arcsin({g} / 10)').evaluate(self.generic_datatable).units, [])
+        self.assertEqual(functions.Function('arccos({g} / 10)').evaluate(self.generic_datatable).units, [])
+        self.assertEqual(functions.Function('arctan({g} / 10)').evaluate(self.generic_datatable).units, [])
+        self.assertEqual(functions.Function('rad{g}').evaluate(self.generic_datatable).units, [])
+        self.assertEqual(functions.Function('deg{g}').evaluate(self.generic_datatable).units, [])
+        self.assertEqual(functions.Function('abs{g}').evaluate(self.generic_datatable).units, self.generic_datatable['g'].units)
+        self.assertEqual(functions.Function('ln{g}').evaluate(self.generic_datatable).units, [])
+        self.assertEqual(functions.Function('log{g}').evaluate(self.generic_datatable).units, [])
+
+    generic_datatable = {'g': functions.Value(9.81, 0.01, False, [(1, 1), (2, 1), (3, -2)]),
+                         'volume': functions.Value(532, 0.1, True, [(2, 3)]),
+                         'mass': functions.Value(25, 1, False, [(1, 1)])}
 
 
 if __name__ == '__main__':
