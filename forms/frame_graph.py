7c9a695e77b34d3e3ccb307099ba1d7fd22b793b
seckujdivad <juckes1david@gmail.com>
2020-02-08 17:22:14 +0000

    Made plot UI

--- forms/frame_graph.py
+++ forms/frame_graph.py
@@ -19,22 +19,46 @@ class GraphFrame(forms.SubFrame):
         self._gbs_main.SetNonFlexibleGrowMode(wx.FLEX_GROWMODE_SPECIFIED)
 
         #create elements
-        self._graph_main = wx.lib.plot.plotcanvas.PlotCanvas(self, wx.ID_ANY)
-        self._graph_main.enableAxes = True
+        self._lb_plots = wx.ListBox(self, wx.ID_ANY)
+        self._gbs_main.Add(self._lb_plots, wx.GBPosition(0, 0), wx.GBSpan(1, 2), wx.ALL | wx.EXPAND)
+
+        self._entry_plot_name = wx.TextCtrl(self, wx.ID_ANY)
+        self._gbs_main.Add(self._entry_plot_name, wx.GBPosition(1, 0), wx.GBSpan(1, 2), wx.ALL | wx.EXPAND)
+
+        self._btn_new_plot = wx.Button(self, wx.ID_ANY, "Add New")
+        self._gbs_main.Add(self._btn_new_plot, wx.GBPosition(2, 0), wx.GBSpan(1, 1), wx.ALL | wx.EXPAND)
+
+        self._btn_remove_plot = wx.Button(self, wx.ID_ANY, "Remove")
+        self._gbs_main.Add(self._btn_remove_plot, wx.GBPosition(2, 1), wx.GBSpan(1, 1), wx.ALL | wx.EXPAND)
+
+        self._lbl_plot_x = wx.StaticText(self, wx.ID_ANY, "x-axis", style = wx.ALIGN_CENTRE_HORIZONTAL)
+        self._gbs_main.Add(self._lbl_plot_x, wx.GBPosition(3, 0), wx.GBSpan(1, 1), wx.ALL | wx.EXPAND)
+
+        self._lb_plot_x = wx.ListBox(self, wx.ID_ANY)
+        self._gbs_main.Add(self._lb_plot_x, wx.GBPosition(4, 0), wx.GBSpan(1, 1), wx.ALL | wx.EXPAND)
+
+        self._lbl_plot_y = wx.StaticText(self, wx.ID_ANY, "y-axis", style = wx.ALIGN_CENTRE_HORIZONTAL)
+        self._gbs_main.Add(self._lbl_plot_y, wx.GBPosition(3, 1), wx.GBSpan(1, 1), wx.ALL | wx.EXPAND)
+
+        self._lb_plot_y = wx.ListBox(self, wx.ID_ANY)
+        self._gbs_main.Add(self._lb_plot_y, wx.GBPosition(4, 1), wx.GBSpan(1, 1), wx.ALL | wx.EXPAND)
+
+        self._plot_main = wx.lib.plot.plotcanvas.PlotCanvas(self, wx.ID_ANY)
+        self._plot_main.enableAxes = True
         data = [[1,5],[2,4],[3,8],[4,3],[5,2]]
         line = wx.lib.plot.PolyLine(data, colour='red', width=1)
         gc = wx.lib.plot.PlotGraphics([line], 'Title', 'x-axis', 'y-axis')
-        self._graph_main.Draw(gc)
-        self._graph_main.enableAntiAliasing = True
-        self._graph_main.enableDrag = True
-        self._graph_main.Bind(wx.EVT_MOUSEWHEEL, self._bind_graph_scroll)
-        self._gbs_main.Add(self._graph_main, wx.GBPosition(0, 0), wx.GBSpan(1, 1), wx.ALL | wx.EXPAND)
+        self._plot_main.Draw(gc)
+        self._plot_main.enableAntiAliasing = True
+        self._plot_main.enableDrag = True
+        self._plot_main.Bind(wx.EVT_MOUSEWHEEL, self._bind_graph_scroll)
+        self._gbs_main.Add(self._plot_main, wx.GBPosition(0, 2), wx.GBSpan(5, 1), wx.ALL | wx.EXPAND)
 
         #set sizer weights
-        for i in [0]:
+        for i in [2]:
             self._gbs_main.AddGrowableCol(i)
         
-        for j in [0]:
+        for j in [0, 4]:
             self._gbs_main.AddGrowableRow(j)
         
         #finalise layout
@@ -50,8 +74,8 @@ class GraphFrame(forms.SubFrame):
         else:
             zoom = 1.1
         
-        centre = (self._graph_main.xCurrentRange[0] + (self._graph_main.xCurrentRange[1] - self._graph_main.xCurrentRange[0]) / 2,
-                  self._graph_main.yCurrentRange[0] + (self._graph_main.yCurrentRange[1] - self._graph_main.yCurrentRange[0]) / 2)
+        centre = (self._plot_main.xCurrentRange[0] + (self._plot_main.xCurrentRange[1] - self._plot_main.xCurrentRange[0]) / 2,
+                  self._plot_main.yCurrentRange[0] + (self._plot_main.yCurrentRange[1] - self._plot_main.yCurrentRange[0]) / 2)
 
-        self._graph_main.Zoom(centre, (zoom, zoom))
+        self._plot_main.Zoom(centre, (zoom, zoom))
         event.Skip()
