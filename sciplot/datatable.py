311c1f383bb990f0202c1b9c8d6225837b78a4d5
seckujdivad <juckes1david@gmail.com>
2020-02-13 00:00:16 +0000

    Wrote processing and loading of dependencies

--- sciplot/datatable.py
+++ sciplot/datatable.py
@@ -17,6 +17,8 @@ class Datatable:
         self._variable_ids = variable_ids.copy()
 
     def load(self, constants_table: typing.Dict[str, functions.Value]):
+        constants_table = constants_table.copy()
+
         var_type_lookup = ["dataset", "formula"]
 
         #get all functions from the database
@@ -28,15 +30,100 @@ class Datatable:
             function_table[name] = func
             function_var_table[variable_id] = func
         
+        #get all datasets from the database
+        dataset_names_ids: typing.Dict[str, int] = {}
+        for dataset_symbol, variable_id in self._datafile.query(database.Query("SELECT Symbol, VariableID FROM Variable WHERE Type = 0", [], 1))[0]:
+            dataset_names_ids[dataset_symbol] = variable_id
+        
+        dependencies = []
+
         #get data on all columns
         variable_data = []
         for variable_id in self._variable_ids:
             variable_symbol, variable_subid, variable_type = self._datafile.query(database.Query("SELECT Symbol, ID, Type FROM Variable WHERE VariableID = (?);", [variable_id], 2))[0]
-            variable_data.append([variable_symbol, variable_subid, variable_type])
+            variable_data.append((variable_id, variable_symbol, variable_subid, variable_type))
+
+            if variable_symbol not in function_table: #is a dataset
+                dependencies.append((variable_symbol, variable_symbol))
+        
+        print(dependencies)
 
         #get formula dependencies
-        dependent_datasets = {}
-        included_datasets = {}
+        for variable_id, variable_symbol, variable_subid, variable_type in variable_data:
+            if var_type_lookup[variable_type] == "formula":
+                for dependency_info in functions.evaluate_dependencies(variable_symbol, function_table):
+                    if dependency_info not in dependencies:
+                        dependencies.append(dependency_info)
+        
+        print(dependencies)
+        
+        #process dependencies
+        dependency_table: typing.Dict[str, typing.Dict[str, object]] = {}
+        for symbol, dependency in dependencies:
+            current_dependency = {"symbol": symbol}
+
+            split_dep_name = dependency.split('.')
+
+            #interpret name
+            if symbol == dependency: #constant, formula or dataset
+                if symbol in function_table: #formula
+                    current_dependency["type"] = "formula"
+                
+                elif dependency in constants_table: #constant
+                    current_dependency["type"] = "constant"
+
+                else: #dataset
+                    current_dependency["type"] = "dataset"
+                    current_dependency["access mode"] = "same row"
+                    current_dependency["processing"] = None
+                    current_dependency["subtype"] = None
+            
+            else: #requires processing
+                if len(split_dep_name) > 2: #graph attribute
+                    if split_dep_name[0] in ["BEST", "WORST", "WORSTMIN", "WORSTMAX"]:
+                        current_dependency["fit line"] = split_dep_name[0].lower()
+
+                    if split_dep_name[1] in ["GRAD", "GRADIENT", "SLOPE"]:
+                        current_dependency["subtype"] = "gradient"
+                    elif split_dep_name[1] in ["INTERCEPT", "Y-INTERCEPT"]:
+                        current_dependency["subtype"] = "intercept"
+
+                else:
+                    current_dependency["type"] = "value"
+
+                    if dependency.endswith(".MEAN"):
+                        current_dependency["access mode"] = "single"
+                        current_dependency["processing"] = "mean"
+                    
+                    elif dependency.endswith(".MAX"):
+                        current_dependency["access mode"] = "single"
+                        current_dependency["processing"] = "max"
+                    
+                    elif dependency.endswith(".MIN"):
+                        current_dependency["access mode"] = "single"
+                        current_dependency["processing"] = "min"
+                
+                    if current_dependency["symbol"] in function_table:
+                        current_dependency["subtype"] = "processed formula"
+                    else:
+                        current_dependency["subtype"] = None
+
+            dependency_table[dependency] = current_dependency
+        
+        print("dependencies:")
+        print(*["{}: {}".format(key, dependency_table[key]) for key in dependency_table], sep = '\n')
+        
+        print("dataset contents:")
+        #get dataset dependencies
+        dataset_table: typing.Dict[str, typing.Union[functions.Value, typing.List[functions.Value]]] = {}
+        for dependency_name in dependency_table:
+            dependency_data = dependency_table[dependency_name]
+            if dependency_data["type"] == "dataset" and dependency_data["subtype"] is None:
+                values = [tup[0] for tup in self._datafile.query(database.Query("SELECT `Value` FROM DataPoint INNER JOIN DataSet ON DataPoint.DataSetID = DataSet.DataSetID INNER JOIN `Variable` ON DataSet.DataSetID = Variable.ID WHERE Symbol = (?) AND Type = 0;", [dependency_data["symbol"]], 1))[0]]
+                print(dependency_data["symbol"], values)
+
+            elif dependency_data["type"] == "graphical":
+                pass
 
     def as_rows(self):
         if len(self._value_table) > 0: #transpose row-column layout
--- tests/test_datatable.py
+++ tests/test_datatable.py
@@ -7,6 +7,7 @@ sys.path.insert(0, up1)
 
 import sciplot.datatable
 import sciplot.datafile
+import sciplot.functions
 
 sys.path.pop(0)
 
@@ -18,8 +19,12 @@ class TestDatatable(unittest.TestCase):
     def create_datatable(self, datafile):
         return sciplot.datatable.Datatable(datafile)
     
-    def test_load(self):
+    def test_load_noerrors(self):
         with self.create_datafile() as datafile:
             datatable = self.create_datatable(datafile)
-            datatable.set_variables([1, 2])
-            datatable.load({})
+            datatable.set_variables([2, 4])
+            datatable.load(self.generic_datatable)
+    
+    generic_datatable = {'g': sciplot.functions.Value(9.81, 0.01, False, [(1, 1), (2, 1), (3, -2)]),
+                         'volume': sciplot.functions.Value(532, 0.1, True, [(2, 3)]),
+                         'mass': sciplot.functions.Value(25, 1, False, [(1, 1)])}
