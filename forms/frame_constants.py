5691344f03b110c9c3f5a90fd5d2d865d8432b0f
seckujdivad <juckes1david@gmail.com>
2020-02-13 17:26:18 +0000

    Wrote constants frame UI

--- forms/frame_constants.py
+++ forms/frame_constants.py
@@ -18,3 +18,47 @@ class ConstantsFrame(forms.SubFrame):
         self._gbs_main.SetNonFlexibleGrowMode(wx.FLEX_GROWMODE_SPECIFIED)
 
         #create elements
+        self._lb_constants = wx.ListBox(self, wx.ID_ANY)
+        self._lb_constants.Bind(wx.EVT_LISTBOX, self._bind_lb_constants_selected)
+        self._gbs_main.Add(self._lb_constants, wx.GBPosition(0, 0), wx.GBSpan(1, 2), wx.ALL | wx.EXPAND)
+
+        self._spn_value = wx.SpinCtrlDouble(self, wx.ID_ANY)
+        self._lb_constants.Bind(wx.EVT_SPINCTRLDOUBLE, self._bind_spn_value_changed)
+        self._gbs_main.Add(self._spn_value, wx.GBPosition(1, 0), wx.GBSpan(1, 2), wx.ALL | wx.EXPAND)
+
+        self._btn_add_new = wx.Button(self, wx.ID_ANY, "Add New")
+        self._lb_constants.Bind(wx.EVT_BUTTON, self._bind_btn_add_new_clicked)
+        self._gbs_main.Add(self._btn_add_new, wx.GBPosition(2, 0), wx.GBSpan(1, 1), wx.ALL | wx.EXPAND)
+
+        self._btn_remove = wx.Button(self, wx.ID_ANY, "Remove")
+        self._lb_constants.Bind(wx.EVT_BUTTON, self._bind_btn_remove_clicked)
+        self._gbs_main.Add(self._btn_remove, wx.GBPosition(2, 1), wx.GBSpan(1, 1), wx.ALL | wx.EXPAND)
+
+        #set sizer weights
+        for i in [0, 1]:
+            self._gbs_main.AddGrowableCol(i)
+        
+        for j in [0]:
+            self._gbs_main.AddGrowableRow(j)
+        
+        #finalise layout
+        self.SetSizer(self._gbs_main)
+        self.Layout()
+        self._gbs_main.Fit(self)
+    
+    #root frame hooks
+
+    #ui binds
+    def _bind_lb_constants_selected(self, event):
+        event.Skip()
+    
+    def _bind_spn_value_changed(self, event):
+        event.Skip()
+
+    def _bind_btn_add_new_clicked(self, event):
+        event.Skip()
+
+    def _bind_btn_remove_clicked(self, event):
+        event.Skip()
+
+    #frame methods
