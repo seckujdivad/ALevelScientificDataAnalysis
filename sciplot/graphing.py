f976084ec773264e51261f3bbf4f8ad9fd7a9592
seckujdivad <juckes1david@gmail.com>
2020-02-14 20:06:43 +0000

    Added worst fits calculations

--- sciplot/graphing.py
+++ sciplot/graphing.py
@@ -1,4 +1,5 @@
 import math
+import typing
 
 
 class FitLines:
@@ -18,13 +19,13 @@ class FitLines:
         self.fit_best_gradient = None
         self.fit_best_intercept = None
 
-        #line with min gradient
-        self.fit_worst_min_gradient = None
-        self.fit_worst_min_intercept = None
-
         #line with max gradient
         self.fit_worst_max_gradient = None
         self.fit_worst_max_intercept = None
+
+        #line with min gradient
+        self.fit_worst_min_gradient = None
+        self.fit_worst_min_intercept = None
     
     def calculate_all(self):
         self.calculate_best_fit()
@@ -54,7 +55,64 @@ class FitLines:
         self.fit_best_intercept = y_mean - (x_mean * self.fit_best_gradient)
 
     def calculate_worst_fits(self):
-        pass
+        x_values, y_values = self._datatable.as_columns()
+
+        potential_fit_lines: typing.List[typing.Tuple[float, float]] = []
+
+        for switch in [(a, b, c, d) for a in [1, -1] for b in [1, -1] for c in [1, -1] for d in [1, -1]]: #compute cartesian product
+            for i in range(len(x_values)):
+                for j in range(len(x_values)):
+                    if i != j:
+                        first_value_x = x_values[i]
+                        first_value_y = y_values[i]
+                        second_value_x = x_values[j]
+                        second_value_y = y_values[j]
+
+                        point_first = [first_value_x.value + (switch[0] * first_value_x.absolute_uncertainty), first_value_y.value + (switch[1] * first_value_y.absolute_uncertainty)]
+                        point_second = [second_value_x.value + (switch[2] * second_value_x.absolute_uncertainty), second_value_y.value + (switch[3] * second_value_y.absolute_uncertainty)]
+                        gradient = (point_second[1] - point_first[1]) / (point_second[0] - point_first[0])
+                        intercept = point_first[1] - (point_first[0] * gradient)
+                        
+                        if self._check_line(gradient, intercept, x_values, y_values):
+                            potential_fit_lines.append((gradient, intercept))
+        
+        if len(potential_fit_lines) == 0:
+            self.fit_worst_max_gradient = None
+            self.fit_worst_max_intercept = None
+            self.fit_worst_min_gradient = None
+            self.fit_worst_min_intercept = None
+
+        else:
+            max_index = 0
+            min_index = 0
+            for i in range(len(potential_fit_lines)):
+                if potential_fit_lines[i][0] > potential_fit_lines[max_index][0]:
+                    max_index = i
+                if potential_fit_lines[i][0] < potential_fit_lines[min_index][0]:
+                    min_index = i
+            
+            self.fit_worst_max_gradient, self.fit_worst_max_intercept = potential_fit_lines[max_index]
+            self.fit_worst_min_gradient, self.fit_worst_min_intercept = potential_fit_lines[min_index]
+
+    def _check_line(self, gradient, intercept, x_values, y_values):
+        for i in range(len(x_values)):
+            if not self._line_covers_value(gradient, intercept, x_values[i], y_values[i]):
+                return False
+        return True
+
+    def _line_covers_value(self, gradient, intercept, x_value, y_value):
+        return self._line_covers_value_axes(gradient, intercept, x_value, y_value) or self._line_covers_value_axes(gradient, intercept, y_value, x_value)
+    
+    def _line_covers_value_axes(self, gradient, intercept, x_value, y_value):
+        min_x = x_value.value - x_value.absolute_uncertainty
+        max_x = x_value.value + x_value.absolute_uncertainty
+        min_y = y_value.value - y_value.absolute_uncertainty
+        max_y = y_value.value + y_value.absolute_uncertainty
+
+        touches_min_x = min_y <= (intercept + (gradient * min_x)) <= max_y
+        touches_max_x = min_y <= (intercept + (gradient * max_x)) <= max_y
+
+        return touches_min_x or touches_max_x
 
     #worst fit properties
     def _get_fit_worst_gradient(self):
