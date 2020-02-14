2e2c0cb752fa0d39598f18ab37fc9f9c457d7a87
seckujdivad <juckes1david@gmail.com>
2020-02-14 00:43:25 +0000

    Wrote calculations of best fits

--- sciplot/graphing.py
+++ sciplot/graphing.py
@@ -1,3 +1,5 @@
+import math
+
 import sciplot.datatable
 
 
@@ -30,8 +32,28 @@ class FitLines:
         self.calculate_best_fit()
         self.calculate_worst_fits()
 
-    def calculate_best_fit(self):
-        pass
+    def calculate_best_fit(self): #https://www.dummies.com/education/math/statistics/how-to-calculate-a-regression-line/
+        #get raw values
+        x_values = [value.value for value in self._datatable.as_columns()[0]]
+        y_values = [value.value for value in self._datatable.as_columns()[1]]
+
+        #calculate means of x and y
+        x_mean = sum(x_values) / len(x_values)
+        y_mean = sum(y_values) / len(y_values)
+
+        #calculate diffsquares and diffs
+        diffsquare_x = sum([pow(value - x_mean, 2) for value in x_values])
+        diffsquare_y = sum([pow(value - y_mean, 2) for value in y_values])
+
+        #calculate standard deviations
+        stdev_x = math.sqrt(diffsquare_x / (len(x_values) - 1))
+        stdev_y = math.sqrt(diffsquare_y / (len(y_values) - 1))
+
+        #calculate sample pearson correlation coefficient https://en.wikipedia.org/wiki/Pearson_correlation_coefficient
+        correl = sum([(x_values[i] - x_mean) * (y_values[i] - y_mean) for i in range(len(x_values))]) / (math.sqrt(diffsquare_x) * math.sqrt(diffsquare_y))
+
+        self.fit_best_gradient = correl * (stdev_y / stdev_x)
+        self.fit_best_intercept = y_mean - (x_mean * self.fit_best_gradient)
 
     def calculate_worst_fits(self):
         pass
