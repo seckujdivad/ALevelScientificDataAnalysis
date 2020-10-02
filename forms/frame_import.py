e54e8195ac2cd0b83415846606a89427a51c16bb
seckujdivad <juckes1david@gmail.com>
2020-10-02 20:57:09 +0100

    Added "import to dataset" buttons to each column when importing a CSV

--- forms/frame_import.py
+++ forms/frame_import.py
@@ -4,6 +4,7 @@ import wx.dataview
 import csv
 import sys
 import typing
+import functools
 
 import forms
 
@@ -26,11 +27,22 @@ class ImportFrame(forms.SubFrame):
         self._gbs_main.SetNonFlexibleGrowMode(wx.FLEX_GROWMODE_SPECIFIED)
 
         #create elements
-        self._imported_data: typing.List[typing.List] = []
+        self._imported_data: typing.List[typing.List[str]] = []
 
         self._btn_choose_csv = wx.Button(self, wx.ID_ANY, "Import CSV")
         self._btn_choose_csv.Bind(wx.EVT_BUTTON, self._bind_btn_choose_csv_clicked)
         self._gbs_main.Add(self._btn_choose_csv, wx.GBPosition(0, 0), wx.GBSpan(1, 1), wx.ALL | wx.EXPAND)
+
+        self._pnl_column_picker = wx.Panel(self, wx.ID_ANY)
+        self._bs_column_picker = wx.BoxSizer(wx.HORIZONTAL)
+
+        self._pnl_column_picker.SetSizer(self._bs_column_picker)
+        self._pnl_column_picker.Layout()
+        self._bs_column_picker.Fit(self._pnl_column_picker)
+
+        self._gbs_main.Add(self._pnl_column_picker, wx.GBPosition(1, 0), wx.GBSpan(1, 1), wx.ALL | wx.EXPAND)
+
+        self._btns_column_pickers: typing.List[wx.Button] = []
 
         self._dvl_data: wx.dataview.DataViewListCtrl = None #DataViewListCtrls can't have their columns dynamically changed, so this is deferred to a method (more about this in forms.frame_data.py)
         self._dvl_data_columns: typing.List[wx.dataview.DataViewColumn] = []
@@ -38,13 +50,13 @@ class ImportFrame(forms.SubFrame):
 
         self._chk_titles_are_included = wx.CheckBox(self, wx.ID_ANY, "Data contains column titles")
         self._chk_titles_are_included.Bind(wx.EVT_CHECKBOX, self._bind_chk_titles_are_included_clicked)
-        self._gbs_main.Add(self._chk_titles_are_included, wx.GBPosition(2, 0), wx.GBSpan(1, 1), wx.ALL | wx.EXPAND)
+        self._gbs_main.Add(self._chk_titles_are_included, wx.GBPosition(3, 0), wx.GBSpan(1, 1), wx.ALL | wx.EXPAND)
 
         #set sizer weights
         for i in [0]:
             self._gbs_main.AddGrowableCol(i)
         
-        for j in [1]:
+        for j in [2]:
             self._gbs_main.AddGrowableRow(j)
         
         #finalise layout
@@ -97,26 +109,31 @@ class ImportFrame(forms.SubFrame):
             style = style | wx.dataview.DV_NO_HEADER
         
         self._dvl_data = wx.dataview.DataViewListCtrl(self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, style)
-        self._gbs_main.Add(self._dvl_data, wx.GBPosition(1, 0), wx.GBSpan(1, 1), wx.ALL | wx.EXPAND)
+        self._gbs_main.Add(self._dvl_data, wx.GBPosition(2, 0), wx.GBSpan(1, 1), wx.ALL | wx.EXPAND)
         self.Layout()
     
     def _display_imported_data(self, data_has_titles = True):
         self._recreate_dvl_data(data_has_titles)
 
         if len(self._imported_data) != 0:
+            num_columns = len(self._imported_data[0])
+
+            #determine column titles
             titles: typing.List = None
             if data_has_titles:
                 titles = self._imported_data[0]
             else:
                 titles = []
-                for i in range(len(self._imported_data[0])):
+                for i in range(num_columns):
                     titles.append("Column {}".format(i))
             
+            #create columns with appropriate titles
             self._dvl_data_columns.clear()
             for title in titles:
                 column = self._dvl_data.AppendTextColumn(title, flags = 0)
                 self._dvl_data_columns.append(column)
             
+            #add csv data to table
             if data_has_titles:
                 start_index = 1
             else:
@@ -126,6 +143,22 @@ class ImportFrame(forms.SubFrame):
                 self._dvl_data.AppendItem(row)
         
             #set column widths
-            column_width = (self._dvl_data.GetSize()[0] - 30) / len(self._dvl_data_columns)
+            column_width = self._dvl_data.GetSize()[0] / len(self._dvl_data_columns)
             for column in self._dvl_data_columns:
                 column.SetWidth(column_width)
+            
+            #create column selection buttons
+            button_num_change = num_columns - len(self._btns_column_pickers)
+            if button_num_change < 0:
+                for i in range(0 - button_num_change):
+                    button = self._btns_column_pickers.pop(-1)
+                    button.Destroy()
+
+            elif button_num_change > 0:
+                for i in range(button_num_change):
+                    button = wx.Button(self._pnl_column_picker, wx.ID_ANY, "Add as dataset")
+                    self._bs_column_picker.Add(button, 1)
+                    self._btns_column_pickers.append(button)
+
+            self._pnl_column_picker.Layout()
+            self.Layout()
